d6b9a56c32ae72dadb2f504b9f993ff07656c6b2
Parker Hodge <iparkerhodge@gmail.com>
2020-05-22 23:28:38 -0500

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Portfolio from './Portfolio';
-
-ReactDOM.render(<Portfolio />, document.getElementById('root'))
-
