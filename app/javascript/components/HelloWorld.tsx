a53de18641e86fdd3768f8c91736328e7fad16d1
Parker Hodge <iparkerhodge@gmail.com>
2023-05-06 19:07:02 -0500

    cleaning;

--- app/javascript/components/HelloWorld.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react"
-
-const HelloWorld: FC<{ greeting: string }> = ({ greeting }) => (
-  <div className="text-red-600">{greeting}</div>
-)
-
-export default HelloWorld
