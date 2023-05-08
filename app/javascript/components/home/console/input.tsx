0ef7367d6d284e35772e0de8f51c5143e3bd0b4f
Parker Hodge <iparkerhodge@gmail.com>
2023-05-08 13:54:14 -0500

    initial console commands

--- app/javascript/components/home/console/input.tsx
+++ app/javascript/components/home/console/input.tsx
@@ -1,23 +1,47 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { Action, consoleReducer, initialConsoleState } from './reducer'
 
 const ConsoleInput: React.FC = () => {
     const [state, dispatch] = useReducer(consoleReducer, initialConsoleState)
     const handleValueChange = (e) => {
-        dispatch(new Action.updateValue(e))
+        dispatch(new Action.UpdateValue(e))
     }
 
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
-            dispatch(new Action.submitInput)
+            dispatch(new Action.SubmitInput)
         }
     }
 
+    useEffect(() => {
+        if (state.submittedInput === undefined) {
+            return
+        }
+
+        const timer = setTimeout(() => {
+            dispatch(new Action.ResponseToInput(state.submittedInput))
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [state.submittedInput])
+
     return (
         <div>
+            {Object.keys(state.logs).length > 0 &&
+                Object.keys(state.logs).map((k) => {
+                    const log = state.logs[k]
+                    if (log === "") {
+                        return <p key={k}>&nbsp;</p>
+                    }
+                    else {
+                        return <div key={k} dangerouslySetInnerHTML={{ __html: log }} />
+                    }
+                })
+            }
             <div className='relative flex w-full'>
-                <p className='z-10'>{'>'}</p>
-                <input className='absolute bg-transparent pl-3 hide-focus w-full' onChange={handleValueChange} onKeyDown={handleKeyDown} />
+                {!state.loading &&
+                    <p className='z-10'>{'>'}</p>
+                }
+                <input className='absolute bg-transparent pl-3 hide-focus w-full' onChange={handleValueChange} onKeyDown={handleKeyDown} value={state.currentValue} />
             </div>
         </div>
     )
--- app/javascript/components/home/console/reducer.ts
+++ app/javascript/components/home/console/reducer.ts
@@ -1,13 +1,19 @@
+import twitter from '../../../../assets/images/twitter.svg'
+import li from '../../../../assets/images/linkedin.svg'
+import gh from '../../../../assets/images/github.svg'
+
 interface ConsoleState {
-    submittedInput: string;
+    submittedInput: string | undefined;
     currentValue: string;
-    logs: {}
+    logs: {},
+    loading: boolean
 }
 
 export const initialConsoleState: ConsoleState = {
-    submittedInput: '',
+    submittedInput: undefined,
     currentValue: '',
-    logs: {}
+    logs: {},
+    loading: false
 }
 
 type ConsoleStateUpdate = (prevState: ConsoleState) => ConsoleState
@@ -23,7 +29,7 @@ interface ConsoleAction {
 }
 
 export namespace Action {
-    export class updateValue implements ConsoleAction {
+    export class UpdateValue implements ConsoleAction {
         e: React.ChangeEvent<HTMLInputElement>
         readonly updateState: ConsoleStateUpdate = (prevState) => {
             return { ...prevState, currentValue: this.e.target.value }
@@ -34,20 +40,85 @@ export namespace Action {
         }
     }
 
-    export class submitInput implements ConsoleAction {
+    export class SubmitInput implements ConsoleAction {
         readonly updateState: ConsoleStateUpdate = (prevState) => {
             let newLogs = prevState.logs
+            let lastLogsIndex: string
+
+            // update logs with user input
             if (Object.keys(newLogs).length === 0) {
-                newLogs['1'] = prevState.currentValue
+                lastLogsIndex = '0'
+                newLogs['1'] = `> ${prevState.currentValue}`
             }
             else {
                 const sortedKeys = Object.keys(newLogs).sort((a: string, b: string) => {
                     return +(a) - +(b)
                 })
-                const lastLogsIndex = sortedKeys.at(-1)
-                newLogs[`${+(lastLogsIndex) + 1}`] = prevState.currentValue
+                lastLogsIndex = sortedKeys.at(-1)
+                newLogs[`${+(lastLogsIndex) + 1}`] = `> ${prevState.currentValue}`
+
             }
-            return { ...prevState, submittedInput: prevState.currentValue, logs: newLogs }
+
+            // sets loading to true. resolved by Action.ResponseToInput
+            return { loading: true, currentValue: '', submittedInput: prevState.currentValue, logs: newLogs }
         }
+    }
+
+    export class ResponseToInput implements ConsoleAction {
+        input: string
+        readonly updateState: ConsoleStateUpdate = (prevState) => {
+            // this action should always set loading: false
+            if (this.input === "clear" || Object.keys(prevState.logs).length === 0) {
+                return { ...prevState, logs: {}, loading: false, submittedInput: undefined }
+            }
+            let newLogs = prevState.logs
+            let lastLogsIndex: string
+
+            const sortedKeys = Object.keys(newLogs).sort((a: string, b: string) => {
+                return +(a) - +(b)
+            })
+            lastLogsIndex = sortedKeys.at(-1)
+            newLogs[`${+(lastLogsIndex) + 1}`] = handleInput(this.input)
+
+            return { ...prevState, logs: newLogs, loading: false, submittedInput: undefined }
+        }
+        constructor(input: string) {
+            this.input = input
+        }
+    }
+}
+
+const handleInput = (input: string): string => {
+    switch (input) {
+        case 'help':
+            return `
+                <p>Options:</p>
+                <br/>
+                <div class='pl-2 grid grid-cols-5'>
+                    <p>- clear</p> <p>clear the terminal</p>
+                </div>
+                <div class='pl-2 grid grid-cols-5'>
+                    <p>- socials</p> <p>list my socials</p>
+                </div>
+            `
+        case 'socials':
+            return `
+                <div class='grid grid-cols-3 py-2'>
+                    <a href='https://twitter.com/parkerhodgedev' target="_blank" class='flex flex-col items-center'>
+                        <img src='${twitter}' class='h-6 w-auto'/>
+                        <p>twitter</p>
+                    </a>
+                    <a href='https://linkedin.com/in/parkerhodgedev' target="_blank" class='flex flex-col items-center'>
+                        <img src='${li}' class='h-6 w-auto'/>
+                        <p>linkedin</p>
+                    </a>
+                    <a href='https://github.com/iparkerhodge' target="_blank" class='flex flex-col items-center'>
+                        <img src='${gh}' class='h-6 w-auto'/>
+                        <p>github</p>
+                    </a>
+                </div>
+            `
+        default:
+            return `<p>Unrecognized command. Type 'help' to see a list of commands.</p>`
     }
 }
