718b5c9e776b8f8f3e26c118ad016fb097496321
Parker Hodge <iparkerhodge@gmail.com>
2023-05-19 13:16:57 -0500

    add work in progress tag to project cars; add leitner project

--- app/javascript/components/projects/components/card/card.tsx
+++ app/javascript/components/projects/components/card/card.tsx
@@ -9,28 +9,36 @@ interface ProjectProps {
         tags?: string[]
         desc?: string
         github: string
+        workInProgress?: boolean
     }
 }
 
 const ProjectCard: React.FC<ProjectProps> = ({ data }) => {
     return (
-        <div className='w-80 h-96 border-2 flex flex-col items-stretch shadow-md rounded-md pb-6'>
-            <a href={data.github} target='_blank' className='cursor-pointer'>
-                <div className='flex justify-center py-2'>
-                    <img src={require(`../../../../../assets/images/${data.img}`)} className='rounded h-40 w-auto' />
-                </div>
-                <h2 className='px-4 font-black text-2xl'>{data.title}</h2>
-                <div className='px-4 py-2'>
-                    <ProjectCardTags tags={data.tags} />
-                    <div>{data.desc}</div>
-                </div>
-            </a>
-            <div className='px-4 h-full flex items-end'>
-                {data.github &&
-                    <a href={data.github} target='_blank'>
-                        <img src={gh} width={20} />
-                    </a>
+        <div className='w-80 h-96 border-2 shadow-md rounded-md pb-6'>
+            <div className='relative'>
+                {data.workInProgress &&
+                    <p className='absolute top-1 right-2 bg-red-600 text-white px-2 rounded-xl text-xs'>Work In Progress</p>
                 }
+            </div>
+            <div className='h-full flex flex-wrap'>
+                <a href={data.github} target='_blank' className='cursor-pointer w-full'>
+                    <div className='flex justify-center py-2'>
+                        <img src={require(`../../../../../assets/images/${data.img}`)} className='rounded h-40 w-auto' />
+                    </div>
+                    <h2 className='px-4 font-black text-2xl'>{data.title}</h2>
+                    <div className='px-4 py-2'>
+                        <ProjectCardTags tags={data.tags} />
+                        <div>{data.desc}</div>
+                    </div>
+                </a>
+                <div className='px-4 flex items-end'>
+                    {data.github &&
+                        <a href={data.github} target='_blank'>
+                            <img src={gh} width={20} />
+                        </a>
+                    }
+                </div>
             </div>
         </div>
     )
